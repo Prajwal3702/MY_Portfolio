1c68b78a43d65d44891720b07d096293fd9c7df9
PrajwalK <prajwalkorade@gmail.com>
2024-10-19 14:48:27 +0530

    media update

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -76,11 +76,11 @@ const Contact = () => {
                     </div>
                 </div>
                 <form onSubmit={onSubmit} className="contact-right">
-                    <label htmlFor="">Your Name</label>
+                    <label >Your Name</label>
                     <input type="text" placeholder='Enter your name' name='name' />
-                    <label htmlFor="">Your Email</label>
+                    <label >Your Email</label>
                     <input type="email" placeholder='Enter your email' name='email' />
-                    <label htmlFor="">Write your message here</label>
+                    <label >Write your message here</label>
                     <textarea name="message" rows="8" placeholder='Enter your message'></textarea>
                     <button type='submit' className="contact-submit">Submit now</button>
                 </form>
