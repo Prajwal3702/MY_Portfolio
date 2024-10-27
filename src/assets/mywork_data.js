a968d756dd09a8e146224d384bb47b5817acf2d5
PrajwalK <prajwalkorade@gmail.com>
2024-10-27 18:19:22 +0530

    update

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -6,27 +6,31 @@ import project5_img from '../assets/project_5.jpg'
 import project6_img from '../assets/project_6.svg'
 
 const mywork_data = [
+
     {
         w_no:1,
         w_name:"Web design",
-        w_img:project1_img
+        w_img:project5_img
     },
+
     {
         w_no:2,
         w_name:"Web design",
-        w_img:project2_img
+        w_img:project3_img
     },
+
     {
         w_no:3,
         w_name:"Web design",
-        w_img:project3_img
+        w_img:project4_img
     },
+
     {
         w_no:4,
         w_name:"Web design",
-        w_img:project4_img
+        w_img:project2_img
     },
-    {
+    /*{
         w_no:5,
         w_name:"Web design",
         w_img:project5_img
@@ -35,7 +39,7 @@ const mywork_data = [
         w_no:6,
         w_name:"Web design",
         w_img:project6_img
-    },
+    },*/
 ]
  
 export default mywork_data;
--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -6,28 +6,28 @@ const Services_Data = [
     },
     {
         s_no:"02",
-        s_name:"Social media",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"03",
         s_name:"Portfolio design",
         s_desc:"Build a Portfolio design with help of Web Technologies like HTML-CSS."
     },
     {
-        s_no:"04",
+        s_no:"03",
         s_name:"Food-Website design",
         s_desc:"Build a food website using Web Technologies."
     },
     {
+        s_no:"04",
+        s_name:"Social media",
+        s_desc:"In Progressss................."
+    },
+    {
         s_no:"05",
-        s_name:"Digital marketing",
-        s_desc:"Web development is the process of building, programming..."
+        //s_name:"Social media",
+        //s_desc:"In Progressss................."
     },
     {
         s_no:"06",
-        s_name:"Content writing",
-        s_desc:"Web development is the process of building, programming..."
+        //s_name:"Content writing",
+        //s_desc:"Web development is the process of building, programming..."
     },
 ]
  
