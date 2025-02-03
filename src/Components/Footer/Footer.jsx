44dc45a2b01b17181eb63ddcc39bdeaf7c8818b0
PrajwalK <prajwalkorade@gmail.com>
2025-02-03 15:42:30 +0530

    update

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,36 +1,63 @@
 //import React from 'react'
-import './Footer.css'
-import footer_logo from '../../assets/logo.png'
-import user_icon from '../../assets/user_icon.svg'
-import AnchorLink from 'react-anchor-link-smooth-scroll'
+import "./Footer.css";
+import footer_logo from "../../assets/logo.png";
+import user_icon from "../../assets/user_icon.svg";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Footer = () => {
   return (
-    <div className='footer'>
-        <div className="footer-top">
-            <div className="footer-top-left">
-                <img src= {footer_logo} alt="" />
-                <p>I am a full stack developer from, Pune recently graduated from Sinhgad Institute Of Technology And Science.</p>
-            </div>
-            <div className="footer-top-right">
-                <div className="footer-email-input">
-                    <img src= {user_icon} alt="" />
-                    <input type="email" placeholder='Enter your email' />
-                </div>
-                <div className="footer-subscribe"><p>Subscribe</p></div>
-            </div>
+    <div className="footer">
+      <div className="footer-top">
+        <div className="footer-top-left">
+          <img src={footer_logo} alt="" />
+          <p>
+            I am a full stack developer from, Pune recently graduated from
+            Sinhgad Institute Of Technology And Science.
+          </p>
         </div>
-        <hr/>
-        <div className="footer-bottom">
-            <p className="footer-bottom-left">© 2024 Prajwal Korade. All rights reserved.</p>
-            <div className="footer-bottom-right">
-                <div className='footer-termservices'><AnchorLink className='anchor-link' offset={50} href='#termservices'><p>Term of Services</p></AnchorLink></div>
-                <div className='footer-privacypolicy'><AnchorLink className='anchor-link' offset={50} href='#privacypolicy'><p>Privacy Policy</p></AnchorLink></div>
-                <div className='footer-connect'><AnchorLink className='anchor-link' offset={50} href='#contact'><p>Connect with me</p></AnchorLink></div>
-            </div>
+        <div className="footer-top-right">
+          <div className="footer-email-input">
+            <img src={user_icon} alt="" />
+            <input type="email" placeholder="Enter your email" />
+          </div>
+          <div className="footer-subscribe">
+            <p>Subscribe</p>
+          </div>
         </div>
+      </div>
+      <hr />
+      <div className="footer-bottom">
+        <p className="footer-bottom-left">
+          © 2024 Prajwal Korade. All rights reserved.
+        </p>
+        <div className="footer-bottom-right">
+          <div className="footer-termservices">
+            <AnchorLink
+              className="anchor-link"
+              offset={50}
+              href="#termservices"
+            >
+              <p>Term of Services</p>
+            </AnchorLink>
+          </div>
+          <div className="footer-privacypolicy">
+            <AnchorLink
+              className="anchor-link"
+              offset={50}
+              href="#privacypolicy"
+            >
+              <p>Privacy Policy</p>
+            </AnchorLink>
+          </div>
+          <div className="footer-connect">
+            <AnchorLink className="anchor-link" offset={50} href="#contact">
+              <p>Connect with me</p>
+            </AnchorLink>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -1,34 +1,34 @@
 const Services_Data = [
     {
-        s_no:"01",
-        s_name:"My Portfolio",
-        s_desc:"My Portfolio website build using React."
+        s_no: "01",
+        s_name: "My Portfolio",
+        s_desc: "My Portfolio website build using React."
     },
     {
-        s_no:"02",
-        s_name:"Portfolio design",
-        s_desc:"Build a Portfolio design with help of Web Technologies like HTML-CSS."
+        s_no: "02",
+        s_name: "Portfolio design",
+        s_desc: "Build a Portfolio design with help of Web Technologies like HTML-CSS."
     },
     {
-        s_no:"03",
-        s_name:"Food-Website design",
-        s_desc:"Build a food website using Web Technologies."
+        s_no: "03",
+        s_name: "Food-Website design",
+        s_desc: "Build a food website using Web Technologies."
     },
     {
-        s_no:"04",
-        s_name:"Social media",
-        s_desc:"In Progressss................."
+        s_no: "04",
+        s_name: "Social media",
+        s_desc: "In Progressss................."
     },
-   /* {
-        //s_no:"05",
-        //s_name:"Social media",
-        //s_desc:"In Progressss................."
-    },
-    {
-       //s_no:"06",
-        //s_name:"Content writing",
-        //s_desc:"Web development is the process of building, programming..."
-    },*/
+    /* {
+         //s_no:"05",
+         //s_name:"Social media",
+         //s_desc:"In Progressss................."
+     },
+     {
+        //s_no:"06",
+         //s_name:"Content writing",
+         //s_desc:"Web development is the process of building, programming..."
+     },*/
 ]
- 
+
 export default Services_Data;
